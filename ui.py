a5c72b4459d26ee1367282848528e4f28263af9c
islam231bi <islambassuni@gmail.com>
2022-05-03 17:20:26 +0200

    added ui file to project

---
+++ ui.py
@@ -0,0 +1,22 @@
+from PySide2.QtCore import QFile, QProcess
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QApplication, QPushButton
+import sys
+
+
+class App:
+    def __init__(self):
+        self.ui = self.loadUiWidget('dictionary.ui')
+        self.ui.show()
+
+    def loadUiWidget(self, uifilename, parent=None):
+        loader = QUiLoader()
+        uifile = QFile(uifilename)
+        uifile.open(QFile.ReadOnly)
+        ui = loader.load(uifile, parent)
+        uifile.close()
+        return ui
+
+app = QApplication(sys.argv)
+a = App()
+app.exec_()
